34f23890092110fac82c4d8a7457b185bcb9651e
gaurav <gjotwani@astreetpress.com>
2014-12-26 20:53:21 -0500

    add class BeerMapClient

---
+++ src/Client/BeerMapClient/BeerMapClient.php
@@ -0,0 +1,47 @@
+<?php
+namespace Client\BeerMapClient;
+use Response\BeerMapResponse;
+
+class BeerMapClient implements BeerMapClientInterface
+{
+    private $apiKey;
+
+    private $apiUrl;
+
+    private $locationNameQuery;
+
+    public function __construct($apiKey, $apiUrl)
+    {
+        $this->apiKey = $apiKey;
+        $this->apiUrl = $apiUrl;
+    }
+
+    public function __get($name)
+    {
+        return property_exists(__CLASS__, $name) ? $this->{$name} : FALSE;
+    }
+
+    public function setLocation($locationNameQuery)
+    {
+        $this->locationNameQuery = $locationNameQuery;
+    }
+
+    public function getResponse()
+    {
+        $rawResponse = $this->getRawResponse();
+        $responseObject = new BeerMapResponse($rawResponse);
+        return $responseObject;
+    }
+
+    public function getRequestUrl()
+    {
+        return trim($this->apiUrl . '/' . $this->apiKey . '/' . urlencode($this->locationNameQuery));
+    }
+
+    public function getRawResponse()
+    {
+        $requestUrl = $this->getRequestUrl();
+        $rawResponse = file_get_contents($requestUrl);
+        return $rawResponse;
+    }
+}

f7e935d4fbddfefeb1c83c41791d60c015ce52b9
gaurav <gjotwani@astreetpress.com>
2014-12-26 21:44:49 -0500

    add testClientInit

--- src/Tests/Client/BeerMapClientTest.php
+++ src/Tests/Client/BeerMapClientTest.php
@@ -5,15 +5,24 @@ use Client\BeerMapClient\BeerMapClient;
 
 class BeerMapClientTest extends \PHPUnit_Framework_TestCase
 {
+    public static $client;
+
+    public static function setUpBeforeClass()
+    {
+        self :: $client = new BeerMapClient(
+            $apiKey = 'not-my-api-key',
+            $apiUrl = 'http://beermapping.com/webservice/loccity'
+        );
+    }
 
     /**
      * Test BeerMapClient init
      */
     public function testClientInit()
     {
-        $client = new BeerMapClient(
-            $apiKey = 'not-my-api-key',
-            $apiUrl = 'http://beermapping.com/webservice/loccity'
+        $this->assertTrue(
+            self :: $client instanceof Client\BeerMapClient\BeerMapClient,
+            'Client isnt of expected type Client\BeerMapClient\BeerMapClient'
         );
     }
 }

7a1d84a0c544d826077f70c73e8dd527d1439269
gaurav <gjotwani@astreetpress.com>
2015-01-16 16:34:31 -0500

    add testResponseContents() with updated assertions

--- src/Tests/Response/BeerMapClientResponse/BeerMapClientResponseTest.php
+++ src/Tests/Response/BeerMapClientResponse/BeerMapClientResponseTest.php
@@ -31,11 +31,16 @@ class BeerMapClientResponseTest extends \PHPUnit_Framework_TestCase
     /**
      * Test Client BeerMap response object
      */
-    public function testGetLocations()
+    public function testResponseContents()
     {
-        $breweries = self :: $beerMapResponse->getBreweries();
-        $this->assertGreaterThan(0, count($breweries));
+        $response = self :: $beerMapResponse->asArray();
 
+        $this->assertArrayHasKey("description", $response);
+        $this->assertArrayHasKey("numFound", $response);
+        $this->assertArrayHasKey("payload", $response);
+        $this->assertGreaterThan(20, count($response["payload"]));
+
+        $breweries = $response["payload"];
         for ($i = 0; $i < 3; $i++) {
             $this->assertArrayHasKey('id', $breweries[$i]);
             $this->assertNotNull('name', $breweries[$i]);
